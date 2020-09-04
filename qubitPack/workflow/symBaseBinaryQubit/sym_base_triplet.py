0c580d97a621221565b7c3e50e3ecf37936f8bf7
jeng-yuantsai <tsaie79@gmail.com>
2020-09-04 16:32:22 -0400

    update

--- qubitPack/workflow/symBaseBinaryQubit/sym_base_triplet.py
+++ qubitPack/workflow/symBaseBinaryQubit/sym_base_triplet.py
@@ -68,17 +68,29 @@ class SymBaseBinaryQubit(DefectWF):
     #     super().__init__(orig_st, natom, defect_type, substitution, distort, vacuum_thickness)
 
     @classmethod
-    def binary_vacancy(cls, cat="c2dbVac"):
-        lpad = LaunchPad.from_file("/home/tug03990/config/project/symBaseBinaryQubit/c2dbVac/my_launchpad.yaml")
-        col = VaspCalcDb.from_db_file("/home/tug03990/config/category/mx2_antisite_pc/db.json").collection
-        # mx2s = col.find({"task_id":{"$in":[3091, 3083, 3093, 3097, 3094, 3102]}})
-        # 3091: S-W, 3083: Se-W, 3093: Te-W, 3097:Mo-S, 3094: Mo-Se, 3102:Mo-Te
-        mx2s = col.find({"task_id":{"$in":[3091, 3083, 3093, 3097, 3094, 3102]}})
+    def binary_vacancy(cls, cat="BN_vac"):
+        lpad = LaunchPad.from_file("/home/tug03990/config/project/symBaseBinaryQubit/BN_vac/my_launchpad.yaml")
+        col = VaspCalcDb.from_db_file('/home/tug03990/atomate/example/config/project/symBaseBinaryQubit/scan_relax_pc/'
+                                      'db.json').collection
 
-        geo_spec = {5* 5 * 3: [15]}
+        mx2s = col.find(
+            {
+                "task_id": {"$in": [1]}
+            }
+        )
+
+        geo_spec = None
         aexx = 0.25
         for mx2 in mx2s:
             pc = Structure.from_dict(mx2["output"]["structure"])
+            if mx2["nsites"] == 2:
+                geo_spec = {25*2: [20]}
+            if mx2["nsites"] == 3:
+                geo_spec = {25*3: [20]}
+            if mx2["nsites"] == 4:
+                geo_spec = {25*4: [20]}
+            if mx2["nsites"] == 8:
+                geo_spec = {25*8: [20]}
 
             defect = ChargedDefectsStructures(pc, antisites_flag=True).defects
 
@@ -87,19 +99,19 @@ class SymBaseBinaryQubit(DefectWF):
             for vac in range(len(defect["substitutions"])):
                 print(cation, anion)
                 # cation vacancy
-                if "{}".format(cation) not in defect["vacancies"][vac]["name"]:
+                if "{}".format("N") not in defect["vacancies"][vac]["name"]:
                     continue
                 for na, thicks in geo_spec.items():
                     for thick in thicks:
-                        for dtort in [0, 0.001]:
+                        for dtort in [0]:
                             vacancy_insttance = cls(pc, na, ("vacancies", vac), False, dtort, thick)
                             wf = get_wf_full_hse(
                                 structure=vacancy_insttance.defect_st,
                                 charge_states=[0],
                                 gamma_only=False,
                                 dos_hse=True,
-                                nupdowns=[2],
-                                encut=320,
+                                nupdowns=[-1],
+                                encut=520,
                                 include_hse_relax=True,
                                 vasptodb={"category": cat, "NN": vacancy_insttance.NN,
                                           "defect_entry": vacancy_insttance.defect_entry},
@@ -122,18 +134,18 @@ class SymBaseBinaryQubit(DefectWF):
                                 }
                                 return kpoints_kwarg
 
-                            wf = add_modify_kpoints(wf, {"kpoints_update": kpoints([1,1,1])}, "PBE_relax")
-                            wf = add_modify_kpoints(wf, {"kpoints_update": kpoints([1,1,1])}, "HSE_relax")
-                            wf = add_modify_kpoints(wf, {"kpoints_update": kpoints([1,1,1])}, "HSE_scf")
+                            # wf = add_modify_kpoints(wf, {"kpoints_update": kpoints([1,1,1])}, "PBE_relax")
+                            # wf = add_modify_kpoints(wf, {"kpoints_update": kpoints([1,1,1])}, "HSE_relax")
+                            # wf = add_modify_kpoints(wf, {"kpoints_update": kpoints([1,1,1])}, "HSE_scf")
                             wf = add_additional_fields_to_taskdocs(
                                 wf,
-                                {"lattice_constant": "HSE",
+                                {"lattice_constant": "SCAN",
                                  "perturbed": vacancy_insttance.distort}
                             )
 
                             wf = add_modify_incar(wf, {"incar_update": {"NSW":150}}, "PBE_relax")
                             wf = add_modify_incar(wf, {"incar_update": {"AEXX":aexx, "NSW":150}}, "HSE_relax")
-                            wf = add_modify_incar(wf, {"incar_update": {"LWAVE": True, "AEXX":aexx}}, "HSE_scf")
+                            wf = add_modify_incar(wf, {"incar_update": {"LWAVE": False, "AEXX":aexx}}, "HSE_scf")
                             wf = set_queue_options(wf, "24:00:00", fw_name_constraint="PBE_relax")
                             wf = set_queue_options(wf, "24:00:00", fw_name_constraint="HSE_relax")
                             wf = set_queue_options(wf, "24:00:00", fw_name_constraint="HSE_scf")
@@ -145,5 +157,5 @@ class SymBaseBinaryQubit(DefectWF):
 
 
 if __name__ == '__main__':
-    relax_pc()
-    # SymBaseBinaryQubit.binary_vacancy()
+    # relax_pc()
+    SymBaseBinaryQubit.binary_vacancy()
