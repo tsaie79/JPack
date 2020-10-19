e7535850c6059a6dce969a02d952ecee12d6c1f3
jeng-yuantsai <tsaie79@gmail.com>
2020-10-19 11:40:08 -0400

    update

--- defectDBPack/defect_db_wf.py
+++ defectDBPack/defect_db_wf.py
@@ -4,6 +4,7 @@ from fireworks import LaunchPad, Workflow
 
 from atomate.vasp.database import VaspCalcDb
 from atomate.vasp.workflows.jcustom.wf_full import get_wf_full_hse, get_wf_full_scan
+from atomate.vasp.workflows.presets.core import wf_bandstructure
 from atomate.vasp.fireworks.core import ScanOptimizeFW
 from atomate.vasp.powerups import (
     add_additional_fields_to_taskdocs,
@@ -16,12 +17,43 @@ from atomate.vasp.powerups import (
 )
 
 from pymatgen import Structure
-from pymatgen.io.vasp.sets import MPScanRelaxSet, MPHSERelaxSet
+from pymatgen.io.vasp.inputs import Kpoints
+from pymatgen.io.vasp.sets import MPScanRelaxSet, MPRelaxSet
+from pymatgen.symmetry.bandstructure import HighSymmKpath
 
 from pycdt.core.defectsmaker import ChargedDefectsStructures
 
 from monty.serialization import loadfn
 import os
+
+
+def test_IR(cat="genWavecar"):
+    lpad = LaunchPad.from_file(
+        os.path.join(
+            os.path.expanduser("~"),
+            "config/project/testIR/{}/my_launchpad.yaml".format(cat)))
+    col = VaspCalcDb.from_db_file(
+        os.path.join(
+            os.path.expanduser("~"),
+            "config/project/symBaseBinaryQubit/scan_relax_pc/db.json")).collection
+
+    input_st = Structure.from_dict(col.find_one({"chemsys":"Mo-S"})["output"]["structure"])
+
+    wf = wf_bandstructure(input_st)
+    for fw_name in ["static", "nscf"]:
+        wf = add_modify_incar(wf, {"incar_update":{"LWAVE":True, "ISYM":2}}, fw_name_constraint=fw_name)
+
+    for ispin in [1,2]:
+        wf = add_modify_incar(wf, {"incar_update":{"ISPIN":ispin}})
+        wf = add_modify_incar(wf)
+        wf = set_execution_options(wf, category=cat)
+        wf = preserve_fworker(wf)
+        wf = set_queue_options(wf, "01:00:00")
+        if ispin == 1:
+            wf.name = "MoS2_spin_{}_2D_k".format("off")
+        elif ispin == 2:
+            wf.name = "MoS2_spin_{}_2D_k".format("on")
+        lpad.add_wf(wf)
 
 
 def relax_pc():
@@ -58,7 +90,7 @@ def relax_pc():
             lpad.add_wf(wf)
 
 
-def binary_scan_defect(cat="binary_defect", impurity_on_nn=None): #BN_vac
+def binary_scan_defect(cat="binary_defect_vac_AB2", impurity_on_nn=None): #BN_vac
     lpad = LaunchPad.from_file(
         os.path.join(
             os.path.expanduser("~"),
@@ -70,13 +102,13 @@ def binary_scan_defect(cat="binary_defect", impurity_on_nn=None): #BN_vac
 
     mx2s = col.find(
         {
-            "nsites": 2
+            "nsites": 3
         }
     )
     geo_spec = None
     aexx = 0.25
     test = []
-    for mx2 in mx2s[1:]:
+    for mx2 in mx2s[45:60]:
         pc = Structure.from_dict(mx2["output"]["structure"])
         if mx2["nsites"] == 2:
             geo_spec = {25*2: [20]}
@@ -122,12 +154,23 @@ def binary_scan_defect(cat="binary_defect", impurity_on_nn=None): #BN_vac
 
                                 defect_data = gen_defect.defect_entry
 
+                                # add charge state regarding nelect
+                                charge, nupdn = None, None
+                                if MPRelaxSet(gen_defect.defect_st).nelect % 2 == 1:
+                                    charge = [1,-1, 0]
+                                    nupdn = [-1,-1,-1]
+                                    print(charge)
+                                    # if odd nelect, it used to be charge=[0]
+                                else:
+                                    charge = [0]
+                                    nupdn = [-1]
+
                                 wf = get_wf_full_scan(
                                     structure=gen_defect.defect_st,
-                                    charge_states=[0],
+                                    charge_states=charge,
                                     gamma_only=False,
                                     dos=True,
-                                    nupdowns=[-1],
+                                    nupdowns=nupdn,
                                     encut=enmax,
                                     vasptodb={
                                         "category": cat,
@@ -182,3 +225,4 @@ def binary_scan_defect(cat="binary_defect", impurity_on_nn=None): #BN_vac
 if __name__ == '__main__':
     # relax_pc()
     binary_scan_defect()
+    # test_IR()
---
+++ qubitPack/workflow/symBaseBinaryQubit/tools.py
@@ -0,0 +1,78 @@
+#%% search_triplet_from_defect_db
+from atomate.vasp.database import VaspCalcDb
+
+from pymatgen import Element
+
+import pandas as pd
+
+db = VaspCalcDb.from_db_file('/Users/jeng-yuantsai/Research/project/symBaseBinaryQubit/calculations/'
+                             'search_triplet_from_defect_db/db.json')
+
+pc_db = VaspCalcDb.from_db_file("/Users/jeng-yuantsai/Research/project/symBaseBinaryQubit/calculations/scan_relax_pc/db.json")
+
+d = []
+
+filter = {
+    "$or":[{"calcs_reversed.output.outcar.total_magnetization":{"$gte":1.5}},
+           {"calcs_reversed.output.outcar.total_magnetization":{"$lte":-1.5}}],
+    "nsites":75,
+    "task_label":"SCAN_scf"
+}
+
+for e in db.collection.find(filter):
+    entry = {}
+    entry["task_id"] = e["task_id"]
+    entry["formula_pretty"] = e["formula_pretty"]
+    entry["mag"] = abs(e["calcs_reversed"][0]["output"]["outcar"]["total_magnetization"])
+    entry["sym_data"] = pc_db.collection.find_one({"task_id":int(e["pc_from"].split("/")[-1])})["sym_data"]["site_symmetry_symbols"]
+    entry["species"] = pc_db.collection.find_one({"task_id":int(e["pc_from"].split("/")[-1])})["sym_data"]["species"]
+    species = list(dict.fromkeys(entry["species"]))
+    entry["elec_configs"] = (Element(species[0]).electronic_structure, Element(species[1]).electronic_structure)
+    entry["group"] = (Element(species[0]).group, Element(species[1]).group)
+    entry["NN"] = e["NN"]
+    entry["bandgap"] = pc_db.collection.find_one({"task_id":int(e["pc_from"].split("/")[-1])})["output"]["bandgap"]
+    entry["pc_from"] = e["pc_from"]
+    entry["a"] = e["input"]["structure"]["lattice"]["a"]
+    entry["b"] = e["input"]["structure"]["lattice"]["b"]
+
+
+
+    d.append(entry)
+
+df = pd.DataFrame(d).sort_values(["sym_data"])# "group", "species"]) #"mag", "pc_from", "bandgap"])
+df = df.set_index("task_id")
+df.to_clipboard()
+
+#%% search for triplet
+from atomate.vasp.database import VaspCalcDb
+
+from pymatgen import Element
+
+import pandas as pd
+
+db = VaspCalcDb.from_db_file('/Users/jeng-yuantsai/Research/project/symBaseBinaryQubit/calculations/'
+                             'search_triplet_from_defect_db/db.json')
+
+es = db.collection.aggregate(
+    [
+        {"$match": {"task_label":"SCAN_scf"}},
+        {"$addFields":{"mag": {"$abs": {"$arrayElemAt": ["$calcs_reversed.output.outcar.total_magnetization",0]}}}},
+        {"$addFields":{"defect_name":"$defect_entry.name", "point_group":"$output.spacegroup.point_group"}},
+        {"$project": {"_id":0, "formula_pretty":1, "nsites":1, "elements":1,
+                      "mag":1, "charge_state":1, "chemsys":1, "defect_name":1,
+                      "point_group":1
+                      }},
+        {"$match":{"mag":{"$gte":1.9, "$lte":2.1}}},
+        {"$sort":{"mag":-1}},
+        # {"$group": {"_id":"$chemsys", "q": {"$push":"$charge_state"},"m": {"$push":"$formula_pretty"}, "nsites":{"$push":"$nsites"}}}
+    ]
+)
+
+ess = []
+for e in es:
+    els = [Element(e["elements"][0]).group, Element(e["elements"][1]).group]
+    els.sort()
+    e.update({"group": "{}-{}".format(els[0], els[1])})
+    # e.pop("elements")
+    ess.append(e)
+df = pd.DataFrame(ess).sort_values(["group","defect_name", "mag"], inplace=False).set_index("formula_pretty")
