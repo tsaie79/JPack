82113a30f564d6621704ec06683a02861f77c89b
jeng-yuantsai <tsaie79@gmail.com>
2021-03-05 14:57:57 -0500

    update

--- projects/ferromag_insulator_Lin/wf.py
+++ projects/ferromag_insulator_Lin/wf.py
@@ -15,26 +15,31 @@ LPAD = LaunchPad.from_file(
 
 f_path = "/home/tug03990/config/project/mag_insulator/slab/structures/tci_slab_hex"
 
-for f in glob.glob(os.path.join(f_path, "*.vasp")):
+for f in glob.glob(os.path.join(f_path, "icsd-157074_mp-169.vasp")):
     print(f.split("/")[-1])
     fws = []
 
     st = Structure.from_file(f)
     encut = 1.3*max([potcar.enmax for potcar in MPRelaxSet(st).potcar])
 
-    override_default_vasp_params={
-        "user_incar_settings": {"EDIFF":1E-4, "EDIFFG":-0.01, "ISIF":3, "SIGMA":0.05, "ISPIN":1, "ENCUT":encut},
-        "vdw": "df"}
+    override_default_vasp_params = {
+        "user_incar_settings": {"EDIFF":1E-4, "EDIFFG":-0.01, "ISIF":3, "SIGMA":0.05, "ISPIN":1,
+                                "ENCUT":encut, "LASPH":False, "ISMEAR":0,
+                                "LCHARG":False},
+        # "user_kpoints_settings": Kpoints.automatic_density_by_vol(st, 6**3),
+        "vdw": None}
 
     vis_set = MPMetalRelaxSet(st, force_gamma=True, **override_default_vasp_params)
     opt_vdw = OptimizeFW(st, vasp_input_set=vis_set, vasp_cmd=">>vasp_cmd<<")
-    opt_vdw = cp_vdw_file(opt_vdw)
 
     hse_static = JHSEStaticFW(
         st, vasp_input_set_params={
-            "user_incar_settings": {"EDIFF":1E-5, "SIGMA":0.05, "ENCUT":encut},
-            "user_kpoints_settings": Kpoints.automatic_density_by_vol(st, 12**3)
+            "user_incar_settings": {"EDIFF":1E-5, "ISMEAR":0, "SIGMA":0.05, "ENCUT":encut,
+                                    "LASPH":False, "ISPIN":1,
+                                    "LVHAR":True, "LCHARG": True},
+            "user_kpoints_settings": Kpoints.automatic_density_by_vol(st, 6**3)
         },
+        vasptodb_kwargs={"parse_chgcar":True},
         parents=opt_vdw
     )
 
@@ -43,12 +48,13 @@ for f in glob.glob(os.path.join(f_path, "*.vasp")):
 
     wf = Workflow(fws, name="{}_{}".format(st.formula, f.split("/")[-1]))
     wf = add_additional_fields_to_taskdocs(wf, {"source": f.split("/")[-1]})
+    opt_vdw = cp_vdw_file(wf, fw_name_constraint=opt_vdw.name)
+
     wf = preserve_fworker(wf)
     wf = set_execution_options(wf, category=CATEGORY)
     wf = add_modify_incar(wf)
 
-    wf = set_queue_options(wf, "32:00:00", fw_name_constraint=opt_vdw.name)
-    wf = set_queue_options(wf, "24:00:00", fw_name_constraint=hse_static.name)
+    wf = set_queue_options(wf, "32:00:00", fw_name_constraint=wf.fws[0].name)
+    wf = set_queue_options(wf, "24:00:00", fw_name_constraint=wf.fws[1].name)
 
     LPAD.add_wf(wf)
-    break
