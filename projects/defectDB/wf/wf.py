416f206c6764d24a4eb64a53b7a4468b3a0d31d0
jeng-yuantsai <tsaie79@gmail.com>
2021-06-17 21:06:35 -0400

    update

--- projects/defectDB/wf/wf.py
+++ projects/defectDB/wf/wf.py
@@ -68,21 +68,23 @@ def bs_pc():
             os.path.expanduser("~"),
             "config/project/defect_db/{}/my_launchpad.yaml".format(cat)))
 
-    col = VaspCalcDb.from_db_file(
-        os.path.join(
-            os.path.expanduser("~"),
-            "config/project/symBaseBinaryQubit/scan_relax_pc/db.json")).collection
+    col = get_db("2dMat_from_cmr_fysik", "2dMaterial_v1", port=12345, user="readUser", password="qiminyan")
 
     mx2s = col.find(
         {
-            "nsites": 2,
+            "gap_hse_nosoc":{"$gt": 0},
+            "nkinds": 2,
+            "magstate": "NM"
         }
     )
 
     for idx, mx2 in enumerate(mx2s[:1]):
         pc = Structure.from_dict(mx2["output"]["structure"])
         pc, site_info = phonopy_structure(pc)
-        wf = get_wf(pc, os.path.join(os.path.dirname(os.path.abspath("__file__")), "projects/defectDB/wf/scan_scf_bs.yaml"))
+        wf = get_wf(pc, os.path.join(os.path.dirname(os.path.abspath("__file__")), "projects/defectDB/wf/scan_full.yaml"))
+
+        wf = remove_todb(wf, wf.fws[0])
+
         wf = add_additional_fields_to_taskdocs(
             wf,
             {
@@ -98,25 +100,38 @@ def bs_pc():
         wf = add_modify_kpoints(
             wf,
             modify_kpoints_params={"kpoints_update": {"kpts": kpt.kpts}},
-            fw_name_constraint="SCAN_scf"
+            fw_name_constraint=wf.fws[2]
         )
         wf = add_modify_twod_bs_kpoints(
             wf,
             modify_kpoints_params={"kpoints_line_density": 10, "reciprocal_density": 144},
-            fw_name_constraint=wf.fws[1].name
+            fw_name_constraint=wf.fws[3]
+        )
+        wf = add_modify_twod_bs_kpoints(
+            wf,
+            modify_kpoints_params={"kpoints_line_density": 10, "reciprocal_density": 144, "mode": "uniform"},
+            fw_name_constraint=wf.fws[4]
         )
 
         if idx % 2 == 1:
             wf = set_execution_options(wf, category=cat, fworker_name="efrc")
         else:
             wf = set_execution_options(wf, category=cat, fworker_name="owls")
-        wf = set_queue_options(wf, "24:00:00")
-        wf = set_queue_options(wf, "06:00:00", fw_name_constraint="SCAN_bs")
-        wf = set_queue_options(wf, "00:30:00", fw_name_constraint="irvsp")
+        wf = set_queue_options(wf, "12:00:00", fw_name_constraint=wf.fws[0].name)
+        wf = set_queue_options(wf, "03:00:00", fw_name_constraint=wf.fws[1].name)
+        wf = set_queue_options(wf, "03:00:00", fw_name_constraint=wf.fws[2].name)
+        wf = set_queue_options(wf, "02:00:00", fw_name_constraint=wf.fws[3].name)
+        wf = set_queue_options(wf, "02:00:00", fw_name_constraint=wf.fws[4].name)
+        wf = set_queue_options(wf, "00:30:00", fw_name_constraint=wf.fws[5].name)
+
         wf = clean_up_files(wf, files=["WAVECAR"], task_name_constraint="IRVSPToDb", fw_name_constraint="irvsp")
-        wf = clean_up_files(wf, files=["CHGCAR*"], fw_name_constraint="SCAN_bs")
+        wf = clean_up_files(wf, files=["CHGCAR*"], fw_name_constraint=wf.fws[3].name)
+        wf = clean_up_files(wf, files=["CHGCAR*"], fw_name_constraint=wf.fws[4].name)
+
+        wf = remove_todb(wf, fw_name_constraint=wf.fws[0].name)
+
         wf = preserve_fworker(wf)
-        wf.name = "{}:SCAN_opt".format(mx2["formula_pretty"])
+        wf.name = "{}:SCAN_full".format(mx2["formula_pretty"])
         lpad.add_wf(wf)
 
 def binary_scan_defect(defect_choice="substitutions", impurity_on_nn=None): #BN_vac
